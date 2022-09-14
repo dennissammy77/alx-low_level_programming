8535ae501cb3d6e28eeb07cf7d430d80502546e9
dennissammy77 <dennissammy77@gmail.com>
2022-09-14 11:48:35 -0700

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main-Entry point
+ * main-Entry point.
  *
- * description-prints alphabet in lowercase followed by an ew line
+ * description-prints alphabet in lowercase followed by an ew line.
  *
- * Return:Always(0) success
+ * Return: Always(0) success.
  *
  */
 
@@ -13,9 +13,9 @@ int main(void)
 {
 	char ch;
 
-	for(ch = 'a'; ch <='z'; ch++)
-		if(ch != 'e' || ch != 'q')
+	for( ch = 'a'; ch <='z'; ch++)
+		if(ch != 'e' && ch != 'q')
 			putchar(ch);			
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
