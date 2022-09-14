4e2511616415cd176ba0290c2ddd4a649cdb514a
dennissammy77 <dennissammy77@gmail.com>
2022-09-14 11:52:15 -0700

    c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main -Entry point.
  *
- * description - prints single digit numbers of bae 10 starting from 0
+ * description - prints single digit numbers of bae 10 starting from 0.
  *
- * Return: Always(0) success
+ * Return: Always(0) success.
  *
- * */
+ */
 
 int main(void)
 {
 	int n;
 
-	for (n=0 ; n <= 9 ; n++)
+	for ( n=0 ; n <= 9 ; n++)
 		putchar(n);
-		putchar('\n');
+
+	putchar('\n');
 	return(0);
 }
