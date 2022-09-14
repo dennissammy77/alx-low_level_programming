c90f44a01b1399a22547f7db2a95a7b0f1ec7677
dennissammy77 <dennissammy77@gmail.com>
2022-09-14 02:49:00 -0700

    c variable

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,9 +16,10 @@ int main(void)
 	for(ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-		for(ch = 'A'; ch <= 'Z'; ch++)
-			putchar(ch);
-			putchar("\n");
+	for(ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar("\n");
 	
 	return(0);
 }
