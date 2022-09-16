d5d2847d61a8b6a9f79b76d924e62584713d9c9f
dennissammy77 <dennissammy77@gmail.com>
2022-09-16 15:13:08 -0700

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,6 +15,7 @@ void times_table(void)
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
+			_putchar(' ');
 
 			prod = num * mult;
 
@@ -26,6 +27,8 @@ void times_table(void)
 			{
 				_putchar((prod / 10) + '0');
 			}
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
