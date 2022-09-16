d62f97d58ff315c0fa29a5a66f947482a6c1edd0
dennissammy77 <dennissammy77@gmail.com>
2022-09-16 15:20:57 -0700

    nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point.
  *
  * Description - adds two integers and returns the result.
  */
-int add(int n,int x);
+int add(int n,int x)
 {
 	int result;
 
