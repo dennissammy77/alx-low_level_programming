14ac90d55ef85a095e3444f1956ed2a8136ccfc5
dennissammy77 <dennissammy77@gmail.com>
2022-09-16 13:32:46 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point,
+ *
+ * description - prints alphabet in lowercase followed by a new line,
+ *
+ * Return: Always(0) success.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+	return (0);
+}
