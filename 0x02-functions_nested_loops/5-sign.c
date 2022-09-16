dd8badac28caef5a14755efec2b870c3f0926801
dennissammy77 <dennissammy77@gmail.com>
2022-09-16 14:31:42 -0700

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,14 @@ int print_sign(int n)
 			return (1);
 			_putchar('+');
 		}
-	else if (n = 0)
+	else if (n == 0)
 		{
 			return (0);
 			_putchar('0');
 		}
 	else 
 		{
+			return (-1);
 			_putchar('-');
 		}
 }
