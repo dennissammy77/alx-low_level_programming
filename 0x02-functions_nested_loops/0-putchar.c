7a66817e43f5bd4cc6fa290bc3bd2e20fce6ad3c
dennissammy77 <dennissammy77@gmail.com>
2022-09-16 12:34:44 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * description - prints _putchar.
+ *
+ * Return: Always(0).
+ *
+ */
+
+int manin(void)
+{
+	printf('_putchar');
+	return(0);
+}
