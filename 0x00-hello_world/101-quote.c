8a4f22dbcac39efbf9b55e823ca0e2ca9f392fd8
dennissammy77 <dennissammy77@gmail.com>
2022-09-21 23:34:24 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
- *  * main-Entry point
+ *  * main - Entry point.
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: Always 1 (Success).
  *     *
  *      */
 int main(void)
 {
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-			fwrite(str, 1);
-				return (1);
+		write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+		return (1);
 }
