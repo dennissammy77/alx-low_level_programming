ca33b554638f99a8be8f84428bd23cfd871cc528
dennissammy77 <dennissammy77@gmail.com>
2022-09-27 13:49:10 -0700

    pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a pointer to a char.
+ * @**s: adress of pointer.
+ * @*to: pointer of char.
+ * Return - no return.
+ */
+void set_string(char **s,chat *to)
+{
+	*s = to;
+}
