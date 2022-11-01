dd938aa59142cbdef6348cf8b25c7a6dec06188f
dennissammy77 <dennissammy77@gmail.com>
2022-11-01 15:45:57 -0700

    bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,13 +11,14 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned int mask;
 
 	if (index > 63)
+	{
 		return (-1);
-
+	}
 	/*create mask that has set bit using bitwise shift*/
 	mask = 1 << index;
 	/*to change value of bit to b,we need to make it 0*/
 	/*then change it to b by useing the OR operand*/
-	*n = (n & ~mask);
+	*n = (*n | mask);
 
 	return (1);
 }
