a41ada644a3dd8da9a4f1e0028bde51fb6d0e075
dennissammy77 <dennissammy77@gmail.com>
2022-11-01 16:07:59 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	}
 
-	val = 0;
+	for (i = 0; b[i];)
+		i++;
 
-	for (i = 0; b[i] != NULL; i++)
-		;
-
-	for (i--; i >= 0; i--)
+	for (i -= 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
